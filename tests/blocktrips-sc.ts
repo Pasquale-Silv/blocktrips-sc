7e5daf4fbf2e572ec79a6c89d664f0afef8fdbe4
Pasquale Silvestre <pasquale.silvestre@live.com>
2024-06-30 18:54:25 +0200

    Anchor SC tests

--- tests/blocktrips-sc.ts
+++ tests/blocktrips-sc.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { BlocktripsSc } from "../target/types/blocktrips_sc";
+import { PublicKey } from "@solana/web3.js";
 
 describe("blocktrips-sc", () => {
   // Configure the client to use the local cluster.
@@ -10,7 +11,12 @@ describe("blocktrips-sc", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const accommodation_business = new PublicKey("5Y88bWffMahHof1unqAc98gf3T1cokUAuEGFtCaetG3v");
+    const date_of_departure = "20240811";
+    const end_date = "20240820";
+    const price = 4;
+
+    const tx = await program.methods.initialize(accommodation_business, date_of_departure, end_date, price).rpc();
     console.log("Your transaction signature", tx);
   });
 });
